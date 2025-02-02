804f8b242b37f0c66771745c5bf3c03a8609af51
Comma <useyourcommas@gmail.com>
2025-02-02 00:43:40 +0000

    main colours

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const bg_main = document.querySelector('.colour_background');
+
+bg_main.addEventListener('click', () => {
+    navigator.clipboard.writeText("#F1E7E5");
+    setTimeout(() => {
+        bg_main.style.backgroundColor = "#EADAD7";
+    }, 0);
+    setTimeout(() => {
+        bg_main.style.backgroundColor = "#F1E7E5";
+    }, 50);
+});
+
+const title_main = document.querySelector('.colour_title');
+
+title_main.addEventListener('click', () => {
+    navigator.clipboard.writeText("#C38783");
+    
+    setTimeout(() => {
+        title_main.style.backgroundColor = "#84645C";
+    }, 0);
+    setTimeout(() => {
+        title_main.style.backgroundColor = "#C38783";
+    }, 50);
+});
+
+const text_main = document.querySelector('.colour_text');
+
+text_main.addEventListener('click', () => {
+    navigator.clipboard.writeText("#906D64");
+
+    setTimeout(() => {
+        text_main.style.backgroundColor = "#785B54";
+    }, 0);
+    setTimeout(() => {
+        text_main.style.backgroundColor = "#906D64";
+    }, 50);
+});
