72c86ee6504c025d6f8e8fce897ca139c67b6d8d
Comma <useyourcommas@gmail.com>
2025-02-02 13:13:43 +0000

    html and css refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-const bg_main = document.querySelector('.colour_background');
-const bg_alt = document.querySelector('.colour_alt_background');
+const bg_main = document.getElementById('.colour_background');
+const bg_alt = document.getElementById('.colour_alt_background');
 
 bg_main.addEventListener('mouseover', () => {
     bg_alt.style.visibility = "visible";
     // refactoring needed
+    
 });
 
 bg_main.addEventListener('click', () => {
@@ -16,7 +17,7 @@ bg_main.addEventListener('click', () => {
     }, 50);
 });
 
-const title_main = document.querySelector('.colour_title');
+const title_main = document.getElementById('.colour_title');
 
 title_main.addEventListener('click', () => {
     navigator.clipboard.writeText("#C38783");
@@ -29,7 +30,7 @@ title_main.addEventListener('click', () => {
     }, 50);
 });
 
-const text_main = document.querySelector('.colour_text');
+const text_main = document.getElementById('.colour_text');
 
 text_main.addEventListener('click', () => {
     navigator.clipboard.writeText("#906D64");
