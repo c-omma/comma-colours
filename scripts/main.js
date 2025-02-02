9b5a6201f8cecb824b7e4c8e4c686c75c4d1f09f
Comma <useyourcommas@gmail.com>
2025-02-02 15:05:56 +0000

    javascript for alternative colours fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
 const main_colors = document.querySelectorAll(".colour");
+const alt_colors = document.querySelectorAll(".colour_alt");
 
 const bg_main = document.getElementById('main_bg');
+const bg_alt = document.getElementById('alt_bg');
+const title_main = document.getElementById('main_title');
+const title_alt = document.getElementById("alt_title");
+const text_main = document.getElementById('main_text');
+const text_alt = document.getElementById('alt_text');
+
+function hide_bg() {
+    bg_alt.style.visibility = "hidden";
+}
+
+function show_bg() {
+    bg_alt.style.visibility = "visible";
+}
+
 bg_main.addEventListener('click', () => {
     navigator.clipboard.writeText("#F1E7E5");
     setTimeout(() => {
@@ -11,16 +26,29 @@ bg_main.addEventListener('click', () => {
     }, 50);
 });
 
-const bg_alt = document.getElementById('alt_bg');
 bg_main.addEventListener('mouseover', () => {
-    bg_alt.style.visibility = "visible";
-    bg_alt.addEventListener('mouseleave', () => {
-        bg_alt.style.visibility = "hidden";
-    });
+    show_bg();
+    bg_alt.addEventListener('mouseleave', hide_bg);
 });
 
+bg_alt.addEventListener('click', () => {
+    navigator.clipboard.writeText("#F9F9F9");
+    setTimeout(() => {
+        alt_colors[0].style.backgroundColor = "#EBEBEB";
+    }, 0);
+    setTimeout(() => {
+        alt_colors[0].style.backgroundColor = "#F9F9F9";
+    }, 50);
+});
 
-const title_main = document.getElementById('main_title');
+function hide_title() {
+    title_alt.style.visibility = "hidden";
+}
+
+function show_title() {
+    title_alt.style.visibility = "visible";
+}
+
 title_main.addEventListener('click', () => {
     navigator.clipboard.writeText("#C38783");
     
@@ -32,15 +60,31 @@ title_main.addEventListener('click', () => {
     }, 50);
 });
 
-const title_alt = document.getElementById("alt_title");
 title_main.addEventListener('mouseover', () => {
-    title_alt.style.visibility = "visible";
-    title_alt.addEventListener('mouseleave', () => {
-        title_alt.style.visibility = "hidden";
-    });
+    show_title();
+    title_alt.addEventListener('mouseleave', hide_title);
 });
 
-const text_main = document.getElementById('main_text');
+
+title_alt.addEventListener('click', () => {
+    navigator.clipboard.writeText("#9EA476");
+    
+    setTimeout(() => {
+        alt_colors[1].style.backgroundColor = "#8C9362";
+    }, 0);
+    setTimeout(() => {
+        alt_colors[1].style.backgroundColor = "#9EA476";
+    }, 50);
+});
+
+function hide_text() {
+    text_alt.style.visibility = "hidden";
+}
+
+function show_text() {
+    text_alt.style.visibility = "visible";
+}
+
 text_main.addEventListener('click', () => {
     navigator.clipboard.writeText("#906D64");
 
@@ -52,10 +96,18 @@ text_main.addEventListener('click', () => {
     }, 50);
 });
 
-const text_alt = document.getElementById('alt_text');
 text_main.addEventListener('mouseover', () => {
-    text_alt.style.visibility = "visible";
-    text_alt.addEventListener('mouseleave', () => {
-        text_alt.style.visibility = "hidden";
-    });
+    show_text();
+    text_alt.addEventListener('mouseleave', hide_text);
+});
+
+text_alt.addEventListener('click', () => {
+    navigator.clipboard.writeText("#F5DAAF");
+    
+    setTimeout(() => {
+        alt_colors[2].style.backgroundColor = "#F2CD92";
+    }, 0);
+    setTimeout(() => {
+        alt_colors[2].style.backgroundColor = "#F5DAAF";
+    }, 50);
 });
